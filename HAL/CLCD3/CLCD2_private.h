bd0d91a478fdded2d18953d7f9ddbdf320be7ad1
mostmahmoud <mostafa.mahmoud.034@h-eng.helwan.edu.eg>
2023-05-22 14:45:48 +0300

    deleted fils

--- HAL/CLCD3/CLCD2_private.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLCD2_PRIVATE_H_
-#define CLCD2_PRIVATE_H_
-
-#define _8_BIT          0
-
-#define _4_BIT          1
-
-
-#endif
--- LIB/BIT_MATH.h
+++
@@ -1,7 +0,0 @@
-#define  SET_BIT(REG,BIT_NO)       REG | (1<<BIT_NO)
-
-#define  CLR_BIT(REG,BIT_NO)       REG & ~(1<<BIT_NO)
-
-#define  TOGGLE_BIT(REG,BIT_NO)    REG ^ (1<<BIT_NO)
-
-#define  GET_BIT(REG,BIT_NO)       (REG>>BIT_NO) & 1
